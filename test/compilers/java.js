3b3ab6bce502986524236ffa32f57a5cc0b92ee6
ajlopez <webmaster@ajlopez.com>
2017-10-02 13:18:23 -0300

    Java Compiler compiles string expression

--- test/compilers/java.js
+++ test/compilers/java.js
@@ -11,3 +11,14 @@ exports['compile integer'] = function (test) {
 	test.ok(result);
 	test.equal(result, '42');
 }
+
+exports['compile string'] = function (test) {
+	var parser = parsers.parser('"foo"');
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile(parser.parseExpression());
+	
+	test.ok(result);
+	test.equal(result, '"foo"');
+}
+
