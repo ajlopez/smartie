782f15793a0a2b7f48902dca0e309f8c395bfee7
ajlopez <webmaster@ajlopez.com>
2017-12-11 09:32:22 -0300

    CSharp compiler compiles simple arithmetic expressions

--- test/compilers/csharp.js
+++ test/compilers/csharp.js
@@ -71,3 +71,73 @@ exports['compile dot'] = function (test) {
 	test.ok(result);
 	test.equal(result, 'a.b');
 }
+
+exports['compile call'] = function (test) {
+	var parser = parsers.parser('add(41, 1)');
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile(parser.parseExpression());
+	
+	test.ok(result);
+	test.equal(result, 'add(41, 1)');
+}
+
+exports['compile return'] = function (test) {
+	var parser = parsers.parser('return;');
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile(parser.parseCommand());
+	
+	test.ok(result);
+	test.equal(result, 'return;');
+}
+
+exports['compile return with expression'] = function (test) {
+	var parser = parsers.parser('return 42;');
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile(parser.parseCommand());
+	
+	test.ok(result);
+	test.equal(result, 'return 42;');
+}
+
+exports['compile add expression'] = function (test) {
+	var parser = parsers.parser('1 + 41');
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile(parser.parseExpression());
+	
+	test.ok(result);
+	test.equal(result, '1 + 41');
+}
+
+exports['compile subtract expression'] = function (test) {
+	var parser = parsers.parser('43 - 1');
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile(parser.parseExpression());
+	
+	test.ok(result);
+	test.equal(result, '43 - 1');
+}
+
+exports['compile multiply expression'] = function (test) {
+	var parser = parsers.parser('21 * 2');
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile(parser.parseExpression());
+	
+	test.ok(result);
+	test.equal(result, '21 * 2');
+}
+
+exports['compile divide expression'] = function (test) {
+	var parser = parsers.parser('84 / 2');
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile(parser.parseExpression());
+	
+	test.ok(result);
+	test.equal(result, '84 / 2');
+}
